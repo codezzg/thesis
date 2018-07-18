add91b7ad842205ed35e439cef539425c500f1aa
silverweed <silverweed1991@gmail.com>
2018-07-18 10:53:01 +0200

    more bugfixes to circular buffer

--- src/common/circular_buffer.hpp
+++ src/common/circular_buffer.hpp
@@ -8,6 +8,15 @@
 #	include "logging.hpp"
 #endif
 
+/** This class implements a FIFO container that supports O(1) insertion and removal,
+ *  as well as efficient and cache-friendly iteration on its elements.
+ *  The max number of elements in this container is fixed and determined by `reserve`,
+ *  but it can be grown while the buffer is being used by calling `reserve` again.
+ *  This container never automatically grows, so it's up to the user to keep track of
+ *  its filling.
+ *  If a new element is added while the buffer is full, the oldest element is overwritten.
+ *  It is a fatal error to pop while there are no elements in this collection.
+ */
 template <typename T>
 class CircularBuffer {
 protected:
@@ -36,23 +45,24 @@ public:
 			memStart = reinterpret_cast<T*>(realloc(memStart, newCap * sizeof(T)));
 		}
 		cap = newCap;
+		elements = std::min(elements, cap);
+		endIdx = elements % cap;
 	}
 
 	~CircularBuffer() { free(memStart); }
 
 	void push_back(const T& elem)
 	{
-#ifndef NDEBUG
-		if (elements == cap) {
-			logging::warn("Warning: overriding element of CircularBuffer! (cap = ", cap, ")");
-		}
-#endif
-		memStart[endIdx] = elem;
-		endIdx = (endIdx + 1) % cap;
 		if (endIdx == startIdx) {
 			// We reached startIdx after looping the buffer: advance it.
 			startIdx = (startIdx + 1) % cap;
+#ifndef NDEBUG
+			logging::warn("Warning: overriding element of CircularBuffer! (cap = ", cap, ")");
+#endif
 		}
+
+		memStart[endIdx] = elem;
+		endIdx = (endIdx + 1) % cap;
 		elements = std::min(elements + 1, cap);
 		assert(startIdx < cap && endIdx < cap && elements <= cap);
 	}
@@ -80,14 +90,16 @@ public:
 
 	struct iter {
 		unsigned idx;
+		std::size_t nElem;
 	};
 
-	iter iter_start() const { return iter{ startIdx }; }
+	iter iter_start() const { return iter{ startIdx, 0 }; }
 	bool iter_next(iter& it, T& value) const
 	{
-		if (it.idx != endIdx) {
+		if (it.nElem < elements) {
 			value = memStart[it.idx];
 			it.idx = (it.idx + 1) % cap;
+			it.nElem++;
 			return true;
 		}
 		return false;
